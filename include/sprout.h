6ab9fcf5293bc765ecd06f936d5639e8a834e74e
sanechka26 <aakifev05@gmail.com>
2025-10-04 13:38:39 +0300

    Add files via upload

---
+++ include/sprout.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+
+int daysToReachHeight(int UpSpeed, int DownSpeed, int DesiredHeight);
---
+++ src/sprout.cpp
@@ -0,0 +1,30 @@
+#include "../include/sprout.h"
+
+int daysToReachHeight(int UpSpeed, int DownSpeed, int DesiredHeight)
+{
+    int day = 0, height = 0;
+
+    if (UpSpeed <= DownSpeed && DesiredHeight > UpSpeed)
+    {
+        return -1;
+    }
+    
+
+    while (height < DesiredHeight)
+    {
+        day++;
+        height += UpSpeed;
+
+        std::cout << "After day -> " << day << " " << height << std::endl;
+        
+        if (height >= DesiredHeight)
+        {
+            return day;
+        }
+        
+        height -= DownSpeed;
+        std::cout << "After night -> " << day << " " << height << std::endl;
+    }
+
+    return day;
+}
